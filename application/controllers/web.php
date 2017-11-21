6592613a9633ae87d49ea7854d3060aa285af7a1
creativehardbeat1 <creativehardbeat1@gmail.com>
2017-11-21 09:21:52 +0700

    Update menu

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -20,16 +20,65 @@ class Web extends CI_Controller {
 		$this->load->view('template/v_header',$data);
 		$this->load->view('v_about',$data);
 		$this->load->view('template/v_footer',$data);	}
+	public function pengguna(){		
+		$data['judul'] = "Referensi Pengguna";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('user/v_user',$data);
+		$this->load->view('template/v_footer',$data);
+	}
 	public function person(){		
-		$data['judul'] = "Halaman Pegawai";
+		$data['judul'] = "Referensi Pegawai";
 		$this->load->view('template/v_header',$data);
 		$this->load->view('person/v_person',$data);
 		$this->load->view('template/v_footer',$data);
 	}
-	public function pengguna(){		
+	public function calon_peserta(){		
+		$data['judul'] = "Daftar Calon Peserta";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('calon_peserta/v_calon_peserta',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function diklat(){		
+		$data['judul'] = "Halaman Diklat";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('diklat/v_diklat',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	
+	public function permohonan_diklat(){		
+		$data['judul'] = "Halaman Permohonan Diklat";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('diklat/v_permohonan_diklat',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function persetujuan_dokumen(){		
 		$data['judul'] = "Halaman Pengguna";
 		$this->load->view('template/v_header',$data);
-		$this->load->view('user/v_user',$data);
+		$this->load->view('persetujuan/v_persetujuan_dokumen',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function persetujuan_peserta(){		
+		$data['judul'] = "Halaman Pegawai";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('persetujuan/v_persetujuan_peserta',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function peserta(){		
+		$data['judul'] = "Halaman Peserta";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('peserta/v_peserta',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function peserta_diklat(){		
+		$data['judul'] = "Halaman Peserta";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('peserta/v_peserta_diklat',$data);
+		$this->load->view('template/v_footer',$data);
+	}
+	public function laporan_diklat(){		
+		$data['judul'] = "Halaman Peserta";
+		$this->load->view('template/v_header',$data);
+		$this->load->view('laporan/v_laporan_diklat',$data);
 		$this->load->view('template/v_footer',$data);
 	}
 }
