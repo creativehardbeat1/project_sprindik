a09a712483f5a5e5c5f5c9c7bd5ba1e4dfe8feb0
purwantoro <purwantoro.amd@gmail.com>
2017-11-22 20:00:37 +0700

    peserta

---
+++ application/controllers/peserta.php
@@ -0,0 +1,90 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Peserta extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('peserta_model','peserta');
+	}
+
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->view('peserta/v_peserta');
+	}
+
+	public function ajax_list()
+	{
+		$list = $this->peserta->get_datatables();
+		$data = array();
+		$no = $_POST['start'];
+		foreach ($list as $peserta) {
+			$no++;
+			$row = array();
+			$row[] = $peserta->id_peserta;
+			$row[] = $peserta->id_daftar_diklat;
+			$row[] = $peserta->flag_approval;
+			$row[] = $peserta->time_creation;
+			$row[] = $peserta->status_peserta;
+			$row[] = $peserta->status_kegiatan;
+
+			//add html for action
+			$row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_peserta('."'".$peserta->id_peserta."'".')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
+				  <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_peserta('."'".$peserta->id_peserta."'".')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
+		
+			$data[] = $row;
+		}
+
+		$output = array(
+						"draw" => $_POST['draw'],
+						"recordsTotal" => $this->peserta->count_all(),
+						"recordsFiltered" => $this->peserta->count_filtered(),
+						"data" => $data,
+				);
+		//output to json format
+		echo json_encode($output);
+	}
+
+	public function ajax_edit($id_peserta)
+	{
+		$data = $this->peserta->get_by_id($id_peserta);
+		echo json_encode($data);
+	}
+
+	public function ajax_add()
+	{
+		$data = array(
+				'id_peserta' => $this->input->post('id_peserta'),
+				'id_daftar_diklat' => $this->input->post('id_daftar_diklat'),
+				'flag_approval' => $this->input->post('flag_approval'),
+				'time_creation' => $this->input->post('time_creation'),
+				'status_peserta' => $this->input->post('status_peserta'),
+				'status_kegiatan' => $this->input->post('status_kegiatan'),
+			);
+		$insert = $this->peserta->save($data);
+		echo json_encode(array("status" => TRUE));
+	}
+
+	public function ajax_update()
+	{
+		$data = array(
+				'id_peserta' => $this->input->post('id_peserta'),
+				'id_daftar_diklat' => $this->input->post('id_daftar_diklat'),
+				'flag_approval' => $this->input->post('flag_approval'),
+				'time_creation' => $this->input->post('time_creation'),
+				'status_peserta' => $this->input->post('status_peserta'),
+				'status_kegiatan' => $this->input->post('status_kegiatan'),
+			);
+		$this->peserta->update(array('id_peserta' => $this->input->post('id_peserta')), $data);
+		echo json_encode(array("status" => TRUE));
+	}
+
+	public function ajax_delete($id_peserta)
+	{
+		$this->peserta->delete_by_id($id_peserta);
+		echo json_encode(array("status" => TRUE));
+	}
+
+}
---
+++ application/views/peserta/v_peserta.php
@@ -0,0 +1,260 @@
+<section>
+	 <div class="container">
+        <h1 style="font-size:20pt">Peserta</h1>
+        <br />
+        <button class="btn btn-success" onclick="add_peserta()"><i class="glyphicon glyphicon-plus"></i> Tambah Peserta</button>
+        <button class="btn btn-default" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i> Reload</button>
+        <br />
+        <br />
+        <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+				
+                    <th>id_peserta</th>
+					<th>id_daftar_diklat</th>
+                    <th>flag_approval</th>
+					<th>time_creation</th>
+                    <th>status_peserta</th>
+					<th>status_kegiatan</th>
+                    <th style="width:125px;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+
+<script src="<?php echo assets_url();?>/datatables/jquery/jquery-2.1.4.min.js"></script>
+<script src="<?php echo assets_url();?>/datatables/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo assets_url();?>/datatables/datatables/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo assets_url();?>/datatables/datatables/js/dataTables.bootstrap.js"></script>
+<script src="<?php echo assets_url();?>/datatables/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+
+
+<script type="text/javascript">
+
+var save_method; //for save method string
+var table;
+
+$(document).ready(function() {
+
+    //datatables
+    table = $('#table').DataTable({ 
+
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('peserta/ajax_list')?>",
+            "type": "POST"
+        },
+
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [ -1 ], //last column
+            "orderable": false, //set not orderable
+        },
+        ],
+
+    });
+
+    //datepicker
+    $('.datepicker').datepicker({
+        autoclose: true,
+        format: "yyyy-mm-dd",
+        todayHighlight: true,
+        orientation: "top auto",
+        todayBtn: true,
+        todayHighlight: true,  
+    });
+
+});
+
+
+
+function add_peserta()
+{
+    save_method = 'add';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+    $('#modal_form').modal('show'); // show bootstrap modal
+    $('.modal-title').text('Tambah Pengguna'); // Set Title to Bootstrap modal title
+}
+
+function edit_peserta(id)
+{
+    save_method = 'update';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+
+    //Ajax Load data from ajax
+    $.ajax({
+        url : "<?php echo site_url('peserta/ajax_edit/')?>/" + id,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+
+            $('[name="id_peserta"]').val(data.id_peserta);
+            $('[name="id_daftar_diklat"]').val(data.id_daftar_diklat);
+			$('[name="flag_approval"]').val(data.flag_approval);
+			$('[name="time_creation"]').val(data.time_creation);
+            $('[name="status_peserta"]').val(data.status_peserta);
+            $('[name="status_kegiatan"]').val(data.status_kegiatan);
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Edit Peserta'); // Set title to Bootstrap modal title
+
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    });
+}
+
+function reload_table()
+{
+    table.ajax.reload(null,false); //reload datatable ajax 
+}
+
+function save()
+{
+    $('#btnSave').text('saving...'); //change button text
+    $('#btnSave').attr('disabled',true); //set button disable 
+    var url;
+
+    if(save_method == 'add') {
+        url = "<?php echo site_url('peserta/ajax_add')?>";
+    } else {
+        url = "<?php echo site_url('peserta/ajax_update')?>";
+    }
+
+    // ajax adding data to database
+    $.ajax({
+        url : url,
+        type: "POST",
+        data: $('#form').serialize(),
+        dataType: "JSON",
+        success: function(data)
+        {
+
+            if(data.status) //if success close modal and reload ajax table
+            {
+                $('#modal_form').modal('hide');
+                reload_table();
+            }
+
+            $('#btnSave').text('save'); //change button text
+            $('#btnSave').attr('disabled',false); //set button enable 
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error adding / update data');
+            $('#btnSave').text('save'); //change button text
+            $('#btnSave').attr('disabled',false); //set button enable 
+
+        }
+    });
+}
+
+function delete_peserta(id)
+{
+    if(confirm('Are you sure delete this data?'))
+    {
+        // ajax delete data to database
+        $.ajax({
+            url : "<?php echo site_url('peserta/ajax_delete')?>/"+id,
+            type: "POST",
+            dataType: "JSON",
+            success: function(data)
+            {
+                //if success reload ajax table
+                $('#modal_form').modal('hide');
+                reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Error deleting data');
+            }
+        });
+
+    }
+}
+
+</script>
+
+<!-- Bootstrap modal -->
+<div class="modal fade" id="modal_form" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title">Form Peserta</h3>
+            </div>
+            <div class="modal-body form">
+                <form action="#" id="form" class="form-horizontal">
+                    <input type="hidden" value="" name="id"/> 
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="control-label col-md-3">ID Peserta</label>
+                            <div class="col-md-9">
+                                <input name="id_peserta" placeholder="ID Peserta" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">ID Pendaftaran</label>
+                            <div class="col-md-9">
+                                <input name="id_daftar_diklat" placeholder="ID Pendaftaran" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Persetujuan</label>
+                            <div class="col-md-9">
+                                <input name="flag_approval" placeholder="Persetujuan" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Time Creation</label>
+                            <div class="col-md-9">
+                                <input name="time_creation" placeholder="Disetujui" class="form-control" type="text"></input>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+						<div class="form-group">
+                            <label class="control-label col-md-3">Status Peserta</label>
+                            <div class="col-md-9">
+                                <input name="status_peserta" placeholder="Status Peserta" class="form-control" type="text"></input>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Status Kegiatan</label>
+                            <div class="col-md-9">
+                                <input name="status_kegiatan" placeholder="Status Kegiatan" class="form-control" type="text"></input>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- End Bootstrap modal -->
+	
+</section>
+		
