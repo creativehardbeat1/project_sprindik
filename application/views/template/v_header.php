0e3258e086dca704fe0bb14c9020a5f4087f890f
creativehardbeat1 <creativehardbeat1@gmail.com>
2017-11-25 10:35:33 +0700

    Update

--- application/views/template/v_header.php
+++ application/views/template/v_header.php
@@ -46,9 +46,12 @@ if(!$user_id){
 		<?php
 		}elseif($status=="2"){ //level pegawai sesuai id_usernya
 		?>
+		<li><a href="<?php echo base_url().index_page();?>/web/pengguna">Pengguna</a></li>
+		<li><a href="<?php echo base_url().index_page();?>/web/calon_peserta">Calon Peserta</a></li>
+		<li><a href="<?php echo base_url().index_page();?>/web/peserta">Peserta</a></li>
+
 		<li><a href="<?php echo base_url().index_page();?>/web/diklat">Diklat</a></li>
 		<li><a href="<?php echo base_url().index_page();?>/web/pegawai">Pegawai</a></li>
-		<li><a href="<?php echo base_url().index_page();?>/web/peserta">Peserta</a></li>
 		<li><a href="<?php echo base_url().index_page();?>/web/persetujuan_dokumen">Persetujuan Dokumen</a></li>
 		<li><a href="<?php echo base_url().index_page();?>/web/persetujuan_peserta">Persetujuan Peserta</a></li>
 		<li><a href="<?php echo base_url().index_page();?>/web/peserta_diklat">Peserta Diklat</a></li>
@@ -58,12 +61,9 @@ if(!$user_id){
 		}elseif($status=="3"){ //level umum sesuai id_usernya
 		?>
 			<li><a href="<?php echo base_url().index_page();?>/web/pengguna">Pengguna</a></li>
-			<!-- <li><a href="<?php echo base_url().index_page();?>/web/profil">Profil</a></li> -->
-			<li><a href="<?php echo base_url().index_page();?>/web/profil">Profil</a></li>
+			<li><a href="<?php echo base_url().index_page();?>/web/profil">Profil Peserta</a></li>
 			<li><a href="<?php echo base_url().index_page();?>/web/diklat">Diklat</a></li>
 			<li><a href="<?php echo base_url().index_page();?>/web/permohonan_diklat">Permohonan Diklat</a></li>
-			<li><a href="<?php echo base_url().index_page();?>/web/calon_peserta">Calon Peserta</a></li>
-			<li><a href="<?php echo base_url().index_page();?>/web/peserta">Peserta</a></li>
 			<li><a href="<?php echo base_url().index_page();?>/web/peserta_diklat">Peserta Diklat
 
 		<?php	
